63030ff3c7b382a8b629610f746afea61a41f245
superphunthyme <paris.estmagik@hotmail.fr>
2014-07-11 17:26:14 -0400

    Changed 'music' content to 'audio' as the former is invalid

--- addon.py
+++ addon.py
@@ -4,7 +4,7 @@ import xbmcplugin
 
 addon_handle = int(sys.argv[1])
 
-xbmcplugin.setContent(addon_handle, 'music')
+xbmcplugin.setContent(addon_handle, 'audio')
 
 url = 'http://playerservices.streamtheworld.com/pls/CBC_R1_OTT_H.pls'
 li = xbmc.ListItem('CBC Radio One',iconImage='CBCLogo.png')
