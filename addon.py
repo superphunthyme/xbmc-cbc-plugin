1121c8bf7734c4d17c6512bb467408e4ebb029a8
superphunthyme <paris.estmagik@hotmail.fr>
2014-07-11 00:04:14 -0400

    Added Logo and initial addon script

---
+++ addon.py
@@ -0,0 +1,13 @@
+import sys
+import xbmcgui
+import sys xbmcplugin
+
+addon_handle = int(sys.argv[1])
+
+xbmcplugin.setContent(addon_handle, 'music')
+
+url = 'http://playerservices.streamtheworld.com/pls/CBC_R1_OTT_H.pls'
+li = xbmc.ListItem('CBC Radio One',iconImage='CBCLogo.png')
+xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+xbmcplugin.endOfDirectory(addon_handle)
