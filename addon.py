330e1600a1756568a0551f03fd147cfe34b68847
superphunthyme <paris.estmagik@hotmail.fr>
2014-08-03 18:34:50 -0400

    Version 0.1.0. First working with all music.cbc.ca links

--- addon.py
+++ addon.py
@@ -1,13 +1,102 @@
 import sys
+import urllib
+import urlparse
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
 
+from resources.lib import CBCParse
+#Note, will need __init__.py
+#sys.path.append(os.path.join(_addon_path, 'resources', 'lib'))
+#import CBCParse
+# getting addon object
+my_addon = xbmcaddon.Addon('plugin.audio.CBCRadio')
+#base url and handle of the addon
+base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
+for arg in args:
+    xbmc.log(arg)
 
 xbmcplugin.setContent(addon_handle, 'songs')
 
-url = 'http://playerservices.streamtheworld.com/pls/CBC_R1_OTT_H.pls'
-li = xbmcgui.ListItem('Radio One', iconImage='CBCLogo.png')
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
+    
+def createListing():
+    listing = []
+    listing.append(('Radio One Ottawa','http://playerservices.streamtheworld.com/pls/CBC_R1_OTT_H.pls','Talk'))
+    listing.append(('Radio Two Ottawa','http://2073.live.streamtheworld.com/CBC_R2_TOR_H','Music Radio'))
+    return listing
+    #listing.append("")
+    
+def populateDirectory(listing):
+    for item in listing:
+        li = xbmcgui.ListItem(item[0],iconImage=my_addon.getAddonInfo('icon'))
+        li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
+        #li.setInfo('music',{'genre':item[2]})
+        xbmcplugin.addDirectoryItem(handle=addon_handle,url=item[1],listitem=li)
+    xbmcplugin.endOfDirectory(addon_handle)    
+    
+mode = args.get('mode',None)
 
-xbmcplugin.endOfDirectory(addon_handle)
+if mode is None:
+    url = build_url({'mode': 'folderEN', 'foldername': 'Folder One'})
+    li = xbmcgui.ListItem('EN', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+    url = build_url({'mode': 'folderFR', 'foldername': 'Folder Two'})
+    li = xbmcgui.ListItem('FR', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
+
+elif mode[0] == 'folderEN':
+    foldername = args['foldername'][0]
+    categories = CBCParse.get_categories()
+    for i in range(0,len(categories)):
+        print categories[i]
+        url = build_url({'mode': "category", 'foldername': categories[i]})
+        li = xbmcgui.ListItem(categories[i], iconImage='DefaultFolder.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
+        #for station in station_names[i]
+        #li = xbmcgui.ListItem(station)
+        
+elif args['foldername'][0] in CBCParse.get_categories():
+    xbmc.log("Entering category " + mode[0])
+    foldername = args['foldername'][0]
+    station_names = CBCParse.get_station_names_by_cat(foldername)
+    for i in range(0,len(station_names)):
+        url = CBCParse.get_station_url_by_station(station_names[i])
+        li = xbmcgui.ListItem(station_names[i], iconImage='DefaultAudio.png')
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+
+#populateDirectory(createListing())
+
+#elif mode[0]=='folder_FR':
+#    foldername = args['foldername'][0]
+#    url = build_url({'mode': 'folder', 'foldername': 'CBC Radio Numbers'})
+#    li = xbmcgui.ListItem('CBC Radio Numbers', iconImage=my_addon.getAddonInfo('icon'))
+#    li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
+#    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+#    xbmcplugin.endOfDirectory(addon_handle)
+
+
+
+#elif mode[0] == 'folderEN':
+#    foldername = args['foldername'][0]
+#    listing = []
+#    listing.append(('Radio One Ottawa','http://playerservices.streamtheworld.com/pls/CBC_R1_OTT_H.pls','Talk'))
+#    listing.append(('Radio Two Ottawa','http://playerservices.streamtheworld.com/pls/CBC_R2_TOR_H.pls','Music Radio'))
+#    for item in listing:
+#        li = xbmcgui.ListItem(item[0])#iconImage=my_addon.getAddonInfo('icon'))
+#        li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
+#        li.setInfo('music',{'genre':item[2]})
+#        xbmcplugin.addDirectoryItem(handle=addon_handle,url=item[1],listitem=li)
+#        
+#    xbmcplugin.endOfDirectory(addon_handle)
+
---
+++ resources/lib/CBCParse.py
@@ -0,0 +1,61 @@
+import CommonFunctions as common
+import os
+
+#getting data to store station info
+dir = os.path.dirname(__file__)
+stations_path = os.path.join(dir,'../data/CBCStations.html')
+station_list_file = open(stations_path,'r')  
+station_list = file.read(station_list_file)
+station_list_file.close()
+
+def get_station_urls(category_titles):
+    urls=[]
+    for i in range(0,len(category_titles)):
+        ret = common.parseDOM(station_list,"div",attrs = {"id":category_titles[i]})	
+        ret2 = common.parseDOM(ret,"span",attrs = {"class":"streamUrl"})
+        urls.append(common.parseDOM(ret2,"a",ret = "href"))
+        print urls[i]
+
+		#return listing
+		#Use ret to narrow in on "a" tag, first with h3, then li
+		#listing[0] is DOM tag, listing[1] is stream url, listing[2] is content-type, not specified here.
+		#if ( == category_titles[i]):
+		
+def get_station_names(category_titles):
+    station_names=[]
+    for i in range(0,len(category_titles)):
+        #taking the block whose id matches the category
+        ret = common.parseDOM(station_list,"div",attrs = {"id":category_titles[i]})
+        station_names.append(common.parseDOM(ret,"a",attrs={"target":"_parent"}))
+        print station_names[i]
+    return station_names
+    
+def get_categories():
+    category_titles = common.parseDOM(station_list, "h3")
+    for i in range(0,len(category_titles)):
+        category_titles[i] = category_titles[i].encode('utf-8')
+    #print category_titles
+    return category_titles
+    
+def get_station_names_by_cat(category_name):
+    station_names=[]
+    ret = common.parseDOM(station_list,"div",attrs={"id":category_name})
+    station_names=(common.parseDOM(ret,"a",attrs={"target":"_parent"}))
+    for i in range(0,len(station_names)):
+        station_names[i] = station_names[i].encode("utf-8")
+    print station_names
+    return station_names
+    
+def get_station_url_by_station(station_name):
+    ret = [x for x in common.parseDOM(station_list,"li") if station_name in x]
+    ret2 = common.parseDOM(ret,"span",attrs = {"class":"streamUrl"})
+    link =(common.parseDOM(ret2,"a",ret = "href"))
+    #print  link[0]
+    return link[0]
+
+    
+#get_station_urls(get_categories())
+#get_station_names(get_categories())
+#get_categories()
+get_station_names_by_cat('COUNTRY')
+#get_station_url_by_station('Piano')
