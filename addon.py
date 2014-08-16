3e4cdc5382fba8b268de856a03a62416d038c0bc
superphunthyme <paris.estmagik@hotmail.fr>
2014-08-16 18:02:05 -0400

    v0.1.1 Links are now directly accessed from list item. Fanart fixed. Remove unused code

--- addon.py
+++ addon.py
@@ -1,9 +1,11 @@
 import sys
 import urllib
+import urllib2
 import urlparse
 import xbmcgui
 import xbmcplugin
 import xbmcaddon
+import re
 
 from resources.lib import CBCParse
 #Note, will need __init__.py
@@ -43,10 +45,12 @@ mode = args.get('mode',None)
 if mode is None:
     url = build_url({'mode': 'folderEN', 'foldername': 'Folder One'})
     li = xbmcgui.ListItem('EN', iconImage='DefaultFolder.png')
+    li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
     url = build_url({'mode': 'folderFR', 'foldername': 'Folder Two'})
     li = xbmcgui.ListItem('FR', iconImage='DefaultFolder.png')
+    li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
     xbmcplugin.endOfDirectory(addon_handle)
@@ -59,6 +63,7 @@ elif mode[0] == 'folderEN':
         print categories[i]
         url = build_url({'mode': "category", 'foldername': categories[i]})
         li = xbmcgui.ListItem(categories[i], iconImage='DefaultFolder.png')
+        li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
     xbmcplugin.endOfDirectory(addon_handle)
         #for station in station_names[i]
@@ -69,9 +74,12 @@ elif args['foldername'][0] in CBCParse.get_categories():
     foldername = args['foldername'][0]
     station_names = CBCParse.get_station_names_by_cat(foldername)
     for i in range(0,len(station_names)):
-        url = CBCParse.get_station_url_by_station(station_names[i])
+        playlist_url = CBCParse.get_station_url_by_station(station_names[i])
+        playlist = urllib2.urlopen(playlist_url)
+        playlist_text = playlist.read()
+        url = re.findall(r"(?<=File1\=).*C",playlist_text)[0]
         li = xbmcgui.ListItem(station_names[i], iconImage='DefaultAudio.png')
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
     xbmcplugin.endOfDirectory(addon_handle)
 
 
