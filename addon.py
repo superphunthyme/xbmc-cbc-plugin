8be26b1d572f7f170906cc60b7195b8ac2eb0432
Isaac <paris.estmagik@hotmail.fr>
2016-02-07 00:03:29 -0500

    Replaced HTML parser with simple JSON parser
    For the moment, this breaks Radio 2 access.
    There is solely Radio 1 access, but 2, 3 and Sonica will be added back

--- addon.py
+++ addon.py
@@ -8,7 +8,7 @@ import xbmcaddon
 import re
 
 #Parsing html (stored locally)
-from resources.lib import CBCParse
+from resources.lib import CBCParse, CBCJsonParser
 #Note, will need __init__.py
 # getting addon object
 my_addon = xbmcaddon.Addon('plugin.audio.CBCRadio')
@@ -27,41 +27,61 @@ def build_url(query):
 mode = args.get('mode',None)
 
 if mode is None:
-    url = build_url({'mode': 'folderEN', 'foldername': 'Folder One'})
-    li = xbmcgui.ListItem('EN', iconImage='DefaultFolder.png')
+    url = build_url({'mode': 'Radio1', 'foldername': 'Folder One'})
+    li = xbmcgui.ListItem('Radio1', iconImage='DefaultFolder.png')
     li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
-    #Plans to add a french directory, maybe for icaimusique.ca
-    #url = build_url({'mode': 'folderFR', 'foldername': 'Folder Two'})
-    #li = xbmcgui.ListItem('FR', iconImage='DefaultFolder.png')
-    #li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
-    #xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    url = build_url({'mode': 'Radio2', 'foldername': 'Folder Two'})
+    li = xbmcgui.ListItem('Radio2', iconImage='DefaultFolder.png')
+    li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
 
     xbmcplugin.endOfDirectory(addon_handle)
 
-#Populates top-level directory with genre categories
-elif mode[0] == 'folderEN':
+# Populates top-level directory with region categories
+elif mode[0] == 'Radio1':
     foldername = args['foldername'][0]
-    categories = CBCParse.get_categories()
-    for i in range(0,len(categories)):
-        print categories[i]
-        url = build_url({'mode': "category", 'foldername': categories[i]})
-        li = xbmcgui.ListItem(categories[i], iconImage='DefaultFolder.png')
+    regions = CBCJsonParser.get_regions('radio1')
+    for region in regions:
+        url = build_url({'mode': "category", 'foldername': region})
+        li = xbmcgui.ListItem(region, iconImage='DefaultFolder.png')
         li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
     xbmcplugin.endOfDirectory(addon_handle)
 
-#Checks if the foldername is a category. If so, populates with all the stations in that category        
-elif args['foldername'][0] in CBCParse.get_categories():
+# Populates top-level directory with genre categories
+elif mode[0] == 'Radio2':
+    foldername = args['foldername'][0]
+    regions = CBCJsonParser.get_regions('radio2')
+    for region in regions:
+        url = build_url({'mode': "category", 'foldername': region})
+        li = xbmcgui.ListItem(region, iconImage='DefaultFolder.png')
+        li.setProperty('fanart_image',my_addon.getAddonInfo('fanart'))
+        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'Radio3':
+    pass
+
+elif mode[0] == 'Sonica':
+    pass
+
+#Checks if the foldername is a category. If so, populates with all the stations in that category
+elif args['foldername'][0] in CBCJsonParser.get_regions('radio1'):
     xbmc.log("Entering category " + mode[0])
     foldername = args['foldername'][0]
-    station_names = CBCParse.get_station_names_by_cat(foldername)
-    for i in range(0,len(station_names)):
-        playlist_url = CBCParse.get_station_url_by_station(station_names[i])
-        playlist = urllib2.urlopen(playlist_url)
-        playlist_text = playlist.read()
-        url = re.findall(r"(?<=File1\=).*C",playlist_text)[0]
-        li = xbmcgui.ListItem(station_names[i], iconImage='DefaultAudio.png')
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+    aac_playlist_url, mp3_playlist_url = CBCJsonParser.get_R1_streams(args['foldername'][0])
+    aac_stream_url = CBCJsonParser.parse_pls(aac_playlist_url)
+    mp3_stream_url = CBCJsonParser.parse_pls(mp3_playlist_url)
+
+    aac_li = xbmcgui.ListItem('High Quality (AAC)', iconImage='DefaultAudio.png')
+    mp3_li = xbmcgui.ListItem('Low Quality (MP3)', iconImage='DefaultAudio.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=aac_stream_url, listitem=aac_li)
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=mp3_stream_url, listitem=mp3_li)
     xbmcplugin.endOfDirectory(addon_handle)
+
+# TODO. Add Soncica and Radio 3
+# Get links working for Radio 2
+# We keep reusing CBCJsonParser.get_regions. Just call it once
